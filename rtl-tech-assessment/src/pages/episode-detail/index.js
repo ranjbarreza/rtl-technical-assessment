c5d17744941709c33969a3007fb54b30f9221768
reza <reza_ranjbar94@yahoo.com>
2020-05-14 15:28:39 +0200

    episode page style

--- rtl-tech-assessment/src/pages/episode-detail/index.js
+++ rtl-tech-assessment/src/pages/episode-detail/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import './style.sass';
 
 export const Episode = () => {
   const { state } = useLocation();
 
-  const { name, summary, image } = state;
+  const { name, summary, image, season, number } = state;
   return (
     <div>
-      <h1>{name}</h1>
-      <p>{summary?.replace('<p>', '').replace('</p>', '')}</p>
-      {image && <img src={image.original} alt={name} />}
+      <section className="title-section">
+        <h1>{name}</h1>
+        <p>
+          Season {season} - Episode {number}
+        </p>
+      </section>
+
+      <section className="section">
+        <p className="episode-description">{summary?.replace('<p>', '').replace('</p>', '')}</p>
+
+        {image && <img className="episode-image" src={image.original} alt={name} />}
+      </section>
     </div>
   );
 };
